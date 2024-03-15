8b67a62f8b0bc59ba2d0078f6f47f408f3869abc
appweb <appweb.live@gmail.com>
2024-03-15 17:01:45 +0530

    changes done for testing

--- controllers/admin/auth.contoller.js
+++ controllers/admin/auth.contoller.js
@@ -215,7 +215,7 @@ const editProfile = async (req, res) => {
 
     if (result) {
       return res.status(200).json({
-        message: "User updated successfully",
+        message: "User updated ffffffff successfully",
       });
     } else {
       return res.status(400).json({
