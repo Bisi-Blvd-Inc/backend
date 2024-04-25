01eb9149bd9458bee2d364901cbf973e0c0947c9
gautammalik <gautammalik.eminence@gmail.com>
2024-04-25 12:04:11 +0530

    worked on stage

--- routes/frontend/personalBudget.router.js
+++ routes/frontend/personalBudget.router.js
@@ -5,6 +5,9 @@ const personalController = require("../../controllers/frontend/personalBudget.co
 router.post("/personal_budget", personalController.personal_budget);
 router.post("/saveBudget", personalController.savePersonalBudget);
 router.get("/getBudget", personalController.getPersonalBudget);
+router.get("/Budget/:id", personalController.getPersonal);
+
+
 router.put("/editPersonalBudget/:id", personalController.editPersonalBudget);
 //
 module.exports = router;
