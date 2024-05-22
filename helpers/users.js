352e13558691c2ee5a43c07e77c08ccf28de1fbc
gautammalik <gautammalik.eminence@gmail.com>
2024-05-22 17:27:47 +0530

    stage push of email template

--- helpers/users.js
+++ helpers/users.js
@@ -120,7 +120,7 @@ const sendstaffMail = async (email,
 };
 
 
-const sendNewuserCreated = async (firstName,resultsArray) => {
+const sendNewuserCreated = async (firstName, resultsArray) => {
 
   try {
     const emailData = {
@@ -141,7 +141,7 @@ const sendNewuserCreated = async (firstName,resultsArray) => {
   }
 };
 
-const sendReturnuser = async (firstName,resultsArray) => {
+const sendReturnuser = async (firstName, resultsArray) => {
 
   try {
     const emailData = {
@@ -162,8 +162,8 @@ const sendReturnuser = async (firstName,resultsArray) => {
   }
 };
 
-const accountDeactivationMail = async (firstname,email) => {
-  
+const accountDeactivationMail = async (firstname, email) => {
+
   try {
     const emailData = {
       email: email,
@@ -182,8 +182,8 @@ const accountDeactivationMail = async (firstname,email) => {
   }
 };
 
-const accountactivationMailToOwner = async (firstname,email) => {
-  
+const accountactivationMailToOwner = async (firstname, email) => {
+
   try {
     const emailData = {
       email: "admin@bisiblvd.com",
@@ -201,8 +201,8 @@ const accountactivationMailToOwner = async (firstname,email) => {
     throw error;
   }
 };
-const accountactivationMail = async (firstname,email) => {
-  
+const accountactivationMail = async (firstname, email) => {
+
   try {
     const emailData = {
       email: email,
@@ -220,8 +220,8 @@ const accountactivationMail = async (firstname,email) => {
     throw error;
   }
 };
-const accountDeactivationMailToOwner = async (firstname,email) => {
-  
+const accountDeactivationMailToOwner = async (firstname, email) => {
+
   try {
     const emailData = {
       email: "admin@bisiblvd.com",
@@ -257,75 +257,77 @@ const sendBookingMail = async (
   endsWith
 ) => {
   try {
-   
+    function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
     var parentDir = path.dirname("api");
     const token = jwt.sign({ email }, process.env.JWT_ACCOUNT_ACTIVATION, {
       expiresIn: "5m",
     });
     const link = `${process.env.FRONT_BASE_URL}/editbooking/${bookingId}`;
-    if(description1.trim()=="" && description2.trim()==""){
+    if (description1.trim() == "" && description2.trim() == "") {
       // means use default template
-        ejs.renderFile(
-              parentDir + "/mail_template/emailtemplate.html",
-              {
-                link: link,
-                email: email,
-                name: name,
-                servicess: servicess,
-                bookingStatusVal: bookingStatusVal,
-                Date: Date,
-                time: time,
-                price: price,
-                ServiceDuration: ServiceDuration,
+      ejs.renderFile(
+        parentDir + "/mail_template/emailtemplate.html",
+        {
+          link: link,
+          email: email,
+          name: capitalizeFirstLetter(name),
+          servicess: servicess,
+          bookingStatusVal: bookingStatusVal,
+          Date: Date,
+          time: time,
+          price: price,
+          ServiceDuration: ServiceDuration,
 
-              },
-              (err, data) => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  const emailData = {
-                    email,
-                    subject: `booking email`,
-                    html: data,
-                  };
-                  return mail.sendMailerHtml(emailData);
-                }
-              }
-            );
+        },
+        (err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            const emailData = {
+              email,
+              subject: `booking email`,
+              html: data,
+            };
+            return mail.sendMailerHtml(emailData);
+          }
+        }
+      );
 
-    }else{
+    } else {
       function replaceStringBetween(str, target, replacement) {
         // If the input string is empty, return an empty string
         if (!str) {
           return '';
         }
-      
+
         let result = str;
-      
+
         // Find all occurrences of the target string (case-sensitive)
         let startIndex = result.indexOf(target);
         let endIndex = startIndex + target.length;
-      
+
         while (startIndex !== -1) {
           // Construct the new string by replacing the substring between the current target occurrence
           const start = result.slice(0, startIndex);
           const middle = replacement;
           const end = result.slice(endIndex);
-      
+
           // Update the result with the replaced string
           result = start + middle + end;
-      
+
           // Find the next occurrence of the target string
           startIndex = result.indexOf(target, endIndex);
           endIndex = startIndex + target.length;
         }
-      
+
         return result;
       }
-      
+
       let description1AfterReplacing = replaceStringBetween(description1, "#SERVICE_NAME#", servicess);
       let description1Final = replaceStringBetween(description1AfterReplacing, "#DATE#", ServiceDuration + " at " + time);
-      
+
       let description2AfterReplacing = replaceStringBetween(description2, "#SERVICE_NAME#", servicess);
       let description2Final = replaceStringBetween(description2AfterReplacing, "#DATE#", ServiceDuration + " at " + time);
       ejs.renderFile(
@@ -333,16 +335,16 @@ const sendBookingMail = async (
         {
           link: link,
           email: email,
-          name: name,
+          name: capitalizeFirstLetter(name),
           servicess: servicess,
           bookingStatusVal: bookingStatusVal,
           Date: Date,
           time: time,
           price: price,
           ServiceDuration: ServiceDuration,
-          description1:description1Final,
-          description2:description2Final,
-          endsWith:endsWith
+          description1: description1Final,
+          description2: description2Final,
+          endsWith: endsWith
 
         },
         (err, data) => {
@@ -359,11 +361,12 @@ const sendBookingMail = async (
         }
       );
     }
-    
+
   } catch (error) {
     throw error;
   }
 };
+
 
 const sendBookingMailExternal = async (
   email,
@@ -374,41 +377,113 @@ const sendBookingMailExternal = async (
   time,
   bookingStatusVal,
   bookingId,
-  bookingPaymentStatus
+  bookingPaymentStatus,
+  description1,
+  description2,
+  endsWith
 ) => {
   try {
+    function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
     var parentDir = path.dirname("api");
     const token = jwt.sign({ email }, process.env.JWT_ACCOUNT_ACTIVATION, {
       expiresIn: "5m",
     });
     const link = `${process.env.FRONT_BASE_URL}/editbooking/${bookingId}`;
-    email
-    ejs.renderFile(
-      parentDir + "/mail_template/emailtemplate1.html",
-      {
-        link: link,
-        email: email,
-        name: name,
-        servicess: servicess,
-        bookingStatusVal: bookingStatusVal,
-        Date: Date,
-        time: time,
-        price: bookingPaymentStatus,
-        ServiceDuration: ServiceDuration
-      },
-      (err, data) => {
-        if (err) {
-          console.log(err);
-        } else {
-          const emailData = {
-            email,
-            subject: `booking email`,
-            html: data,
-          };
-          return mail.sendMailerHtml(emailData);
+    if (description1.trim() == "" && description2.trim() == "") {
+      ejs.renderFile(
+        parentDir + "/mail_template/emailtemplate1.html",
+        {
+          link: link,
+          email: email,
+          name: capitalizeFirstLetter(name),
+          servicess: servicess,
+          bookingStatusVal: bookingStatusVal,
+          Date: Date,
+          time: time,
+          price: bookingPaymentStatus,
+          ServiceDuration: ServiceDuration
+        },
+        (err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            const emailData = {
+              email,
+              subject: `booking email`,
+              html: data,
+            };
+            return mail.sendMailerHtml(emailData);
+          }
         }
+      );
+    }else {
+      function replaceStringBetween(str, target, replacement) {
+        // If the input string is empty, return an empty string
+        if (!str) {
+          return '';
+        }
+
+        let result = str;
+
+        // Find all occurrences of the target string (case-sensitive)
+        let startIndex = result.indexOf(target);
+        let endIndex = startIndex + target.length;
+
+        while (startIndex !== -1) {
+          // Construct the new string by replacing the substring between the current target occurrence
+          const start = result.slice(0, startIndex);
+          const middle = replacement;
+          const end = result.slice(endIndex);
+
+          // Update the result with the replaced string
+          result = start + middle + end;
+
+          // Find the next occurrence of the target string
+          startIndex = result.indexOf(target, endIndex);
+          endIndex = startIndex + target.length;
+        }
+
+        return result;
       }
-    );
+      let description1AfterReplacing = replaceStringBetween(description1, "#SERVICE_NAME#", servicess);
+      let description1Final = replaceStringBetween(description1AfterReplacing, "#DATE#", ServiceDuration + " at " + time);
+
+      let description2AfterReplacing = replaceStringBetween(description2, "#SERVICE_NAME#", servicess);
+      let description2Final = replaceStringBetween(description2AfterReplacing, "#DATE#", ServiceDuration + " at " + time);
+
+      ejs.renderFile(
+        parentDir + "/mail_template/emialexternalcustom.html",
+        {
+          link: link,
+          email: email,
+          name: capitalizeFirstLetter(name),
+          servicess: servicess,
+          bookingStatusVal: bookingStatusVal,
+          Date: Date,
+          time: time,
+          price: bookingPaymentStatus,
+          ServiceDuration: ServiceDuration,
+          description1: description1Final,
+          description2: description2Final,
+          endsWith: endsWith
+        },
+        (err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            const emailData = {
+              email,
+              subject: `booking email`,
+              html: data,
+            };
+            return mail.sendMailerHtml(emailData);
+          }
+        }
+      );
+    }
+
   } catch (error) {
     throw error;
   }
