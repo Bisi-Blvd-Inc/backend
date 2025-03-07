ba5f884bb9dce06a721ea2e5ff94f34be4240477
Udazi Ephraim Oseiwe <udazi.osebest@gmail.com>
2025-03-07 18:19:27 +0100

    Product api update to accept more fields

--- routes/frontend/inventory.router.js
+++ routes/frontend/inventory.router.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const inventoryController = require("../../controllers/frontend/inventory.controller");
 const upload = require("../../middlewares/multer")
 
-router.post("/create",upload.single("productImg") ,inventoryController.createInventory);
+router.post("/create",upload.array("productImgs", 3) ,inventoryController.createInventory);
 router.get("/get", inventoryController.getAllInventory);
 router.get("/get/:id", inventoryController.getSingleInventory);
-router.put("/edit/:id",upload.single("productImg"), inventoryController.editInventory);
+router.put("/edit/:id",upload.array("productImgs", 3), inventoryController.editInventory);
 router.delete("/removeById/:id", inventoryController.deleteInventory);
 router.post("/multiRemove", inventoryController.multiDeleteInventory);
 router.get("/search", inventoryController.searchInventory);
