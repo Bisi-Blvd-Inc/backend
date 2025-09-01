8172521079141902ab125294e124abf3f2cdcf2f
Abeeb Oladele <oladele_abeeb@stearsng.com>
2025-09-02 00:22:29 +0100

    fix: fixes authentication and appointment history bugs

--- controllers/frontend/auth.controller.js
+++ controllers/frontend/auth.controller.js
@@ -30,7 +30,7 @@ const signin = async (req, res) => {
         });
       }
 
-      if (user.role !== 2) {
+      if (![2, 3].includes(user.role)) {
         return res.status(203).send({
           message: "Access denied!",
           status: 403,
@@ -444,7 +444,7 @@ const autoSignIn = async (req, res) => {
         });
       }
 
-      if (user.role !== 2) {
+      if (![2, 3].includes(user.role)) {
         return res.status(203).send({
           message: "Access denied!",
           status: 403,
