5943fe6bb47d0c88571d1aa71d3674d1457cf2a8
hannahm-b <hannah.m@getbisibooks.com>
2026-07-28 17:22:55 -0500

    Update searchSoaps to filter by addedBy user
    Filter soap search results by logged-in user to prevent cross-account data sharing

--- controllers/frontend/soap.controller.js
+++ controllers/frontend/soap.controller.js
@@ -85,10 +85,7 @@ const searchSoaps = async (req, res) => {
   try {
     // const { subjective, date } = req.params;
     const { subjective } = req.params;
-
-    // const soaps = await usersoap.find({ subjective: new RegExp(subjective, 'i'), createdAt: { $gte: date } });
-    const soaps = await usersoap.find({ subjective: new RegExp(subjective, 'i') });
-    
+    const soaps = await usersoap.find({ subjective: new RegExp(subjective, 'i'), addedBy: req._user });
     res.status(200).json({ msg: soaps });
   } catch (error) {
     console.error("Error searching soaps:", error);
