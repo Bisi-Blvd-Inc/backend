5ccd2ec790aaed20d2b972a5dd44ee4d61c4e2ea
hannahm-b <hannah.m@getbisibooks.com>
2026-08-13 15:50:01 -0500

    Update response property for deletion confirmation

--- controllers/admin/businessService.controller.js
+++ controllers/admin/businessService.controller.js
@@ -160,7 +160,7 @@ const deleteBusinessService = async (req, res) => {
       { $set: { isDeleted: true } }
     );
 
-    if (response.nModified > 0) {
+    if (response.modifiedCount > 0) {
       return res.status(200).json({
         success: true,
         message: "Business Service Deleted Successfully",
